27e0b3cd004147b4628d79d57d48eb5396af9753
Simran145678 <hkaur607@my.centennialcollege.ca>
2020-10-29 22:33:59 -0400

    added URI

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
-module.exports= 
-{
-    "URI":"mongodb://localhost/book_store"
-}
+module.exports = {
+  // "URI":"mongodb://localhost/book_store"
+  URI:
+    "mongodb+srv://Simran145678:oBR3FwAJJfFJBRvk@mongodbserver.k05wl.mongodb.net/book_store?retryWrites=true&w=majority",
+};
